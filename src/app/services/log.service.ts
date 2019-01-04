60bace7b77bdc31540a8d8cc0c230f486219ecd2
anish kumar <anishkumar92@gmail.com>
2019-01-04 14:57:31 +0530

    log services set

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject,Observable,of} from 'rxjs';
 import {Log} from '../models/Log';
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
   logs:Log[];
+
+      private logSource= new BehaviorSubject<Log>({id: null,text :null,date:null});
+    selectedLog = this.logSource.asObservable();
   constructor() {
     this.logs = [
       {id:'1',text:'Generated Components',date:new Date('12/26/2017 12:54:23')},
@@ -13,7 +17,12 @@ export class LogService {
     ]
    }
 
-   getLogs(){
-     return this.logs;
+   getLogs():Observable<Log[]>{
+     return of(this.logs);
    }
+
+   setFormLog(log:Log){
+     this.logSource.next(log);
+   }
+   
 }
